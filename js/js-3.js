e6598833775715554eb38cb48ff63d87c4e3508c
Syohei YOSHIDA <syohex@gmail.com>
2015-07-22 21:17:33 +0900

    Implement JS problem 3

--- js/js-3.js
+++ js/js-3.js
@@ -1,1 +1,11 @@
 // 課題 JS-3 の実装をここに記述してください。
+(function() {
+    var btn = document.getElementById("submit-button");
+    btn.addEventListener("click", function() {
+        var content = document.getElementById("log-input");
+        var ltsv = parseLTSVLog(content.value);
+        var tc = document.getElementById("table-container");
+        createLogTable(tc, ltsv);
+        return true;
+    }, false);
+}());
